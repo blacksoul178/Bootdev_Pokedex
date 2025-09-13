6ed4cf21dd41c44dac73eb72ca443804847e0cba
Marc-André <blacksoul178@gmail.com>
2025-09-13 12:11:43 -0400

    Exiot cmd

--- main.go
+++ main.go
@@ -13,15 +13,47 @@ func cleanInput(text string) []string {
 	return split_text
 }
 
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	// "help":{
+	// 	name:  "help",
+	// 	description: "returns help for available commands",
+	// 	callback: commandHelp,
+
+	// },
+}
+
 func main() {
 	fmt.Print("welcome to the Boot.Dev Pokedex!, please enter a search.\n")
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex>")
 		scanner.Scan()
 		text := scanner.Text()
 		cleaned := cleanInput(text)
-		fmt.Printf("Your command was: %v\n", cleaned[0])
+		switch cleaned[0] {
+		case "exit":
+			commands["exit"].callback()
+		default:
+			fmt.Print("Unknown Command\n")
+		}
 
 	}
 
