3b7fe51c355e9826798b309934569e6784960ba8
Marc-André <blacksoul178@gmail.com>
2025-09-16 15:37:34 -0400

    half of caching done

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -14,3 +14,50 @@ type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
 }
+
+func NewCache(cleanupInterval time.Duration) *Cache {
+	c := &Cache{
+		Entries: make(map[string]cacheEntry),
+	}
+
+	go func() {
+		ticker := time.NewTicker(cleanupInterval)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			c.reapLoop()
+		}
+	}()
+	return c
+}
+
+func (c *Cache) reapLoop() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	expiration := 5 * time.Minute
+	now := time.Now()
+
+	for key, entry := range c.Entries {
+		if now.Sub(entry.createdAt) > expiration {
+			delete(c.Entries, key)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       value,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	if value, ok := c.Entries[key]; ok {
+		return value.val, true
+	} else {
+		return nil, false
+	}
+}
